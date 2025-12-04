42d0b418e2105cce829df2fa5f86213ac0745763
PixelNoah-ui <pixelnoah8@gmail.com>
2025-12-04 08:36:35 +0300

    update cors on index.ts to allow frontend url

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://brothers-tlcn.vercel.app/",
     credentials: true,
   })
 );
