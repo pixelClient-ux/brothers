bef267f33f1320807d788c7fd5b7a377b83b98e5
pixelClient-ux <aliorder235@gmail.com>
2025-12-05 11:02:35 +0300

    Update useLogin.ts

--- client/src/hooks/useLogin.ts
+++ client/src/hooks/useLogin.ts
@@ -1,17 +1,17 @@
 import login from "@/app/(api)/login";
 import { useMutation } from "@tanstack/react-query";
-// import { useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 export interface loginType {
   email: string;
   password: string;
 }
 export default function useLogin() {
-  // const router = useRouter();
+  const router = useRouter();
   const { mutate, isPending } = useMutation({
     mutationFn: (data: loginType) => login(data),
     onSuccess: () => {
-      // router.push("/");
+      router.push("/");
     },
     onError: (error) => {
       toast.error(error.message || "Somethin went wrong, please try agin");
